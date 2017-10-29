5e4cca96cf65077922036eba80e97c54a5f80375
Bo Wang <bo@yieldmo.com>
2017-10-28 20:54:05 -0400

    formats interior page

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,14 @@ $(function() {
       navigation: require('./modules/_navigation.js')
     },
 
+    registerEvents: function() {
+      $("#arrowDown").on("click", this.onArrowClick);
+      $("#sectionFormats").on("scroll", this.onScroll);
+    },
+
     initSlickSlider: function() {
       /* http://kenwheeler.github.io/slick/#settings */
-      $('.hero-carousel').slick({
+      $('#heroCarousel').slick({
         dots: true,
         arrows: false,
         // autoplay: true,
@@ -15,8 +20,27 @@ $(function() {
       });
     },
 
+    onArrowClick: function() {
+      $("html, body").animate({ scrollTop: $("#heroCarousel").position().top }, 600);
+    },
+
+    onScroll: function() {
+      var currentScrollTop = $(this).scrollTop();
+      var viewportHeight = $(this).height();
+      var pageNumber = parseInt((currentScrollTop)/viewportHeight);
+
+      // console.log(currentScrollTop, pageNumber);
+
+      if (pageNumber !== Number(this.dataset.page)) {
+        $(this).removeClass('active-' + this.dataset.page);
+        $(this).addClass('active-' + pageNumber);
+        $(this).attr("data-page", pageNumber);
+      }
+    },
+
     init: function() {
       this.modules.navigation();
+      this.registerEvents();
 
       if (window.section === 'home') {
         this.initSlickSlider();
